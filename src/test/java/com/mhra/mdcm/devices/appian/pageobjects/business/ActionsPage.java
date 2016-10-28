cd3d6f5365d5d7ed2a22fd89d36be4a36251429a
TPD_Auto <nooruddin1504@aol.com>
2016-10-28 15:05:54 +0100

    Wait for test harness page

--- src/test/java/com/mhra/mdcm/devices/appian/pageobjects/business/ActionsPage.java
+++ src/test/java/com/mhra/mdcm/devices/appian/pageobjects/business/ActionsPage.java
@@ -3,6 +3,7 @@ package com.mhra.mdcm.devices.appian.pageobjects.business;
 
 import com.mhra.mdcm.devices.appian.pageobjects._Page;
 import com.mhra.mdcm.devices.appian.pageobjects.business.sections.CreateTestsData;
+import com.mhra.mdcm.devices.appian.utils.selenium.page.PageUtils;
 import com.mhra.mdcm.devices.appian.utils.selenium.page.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -36,8 +37,9 @@ public class ActionsPage extends _Page {
 
     public CreateTestsData gotoTestsHarnessPage() {
         WaitUtils.waitForElementToBePartOfDOM(driver, By.partialLinkText("Create Test Account"), TIMEOUT_MEDIUM, false);
-        //WaitUtils.waitForElementToBeClickable(driver, linkCreateTestAccount, TIMEOUT_MEDIUM, false);
-        linkCreateTestAccount.click();
+        WaitUtils.waitForElementToBeClickable(driver, linkCreateTestAccount, TIMEOUT_MEDIUM, false);
+        //linkCreateTestAccount.click();
+        PageUtils.singleClick(driver, linkCreateTestAccount);
         return new CreateTestsData(driver);
     }
 }
