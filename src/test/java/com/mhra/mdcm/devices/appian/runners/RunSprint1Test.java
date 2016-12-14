8cf89cd306fa13793987724df61fbd7a6cf2734d
TPD_Auto <nooruddin1504@aol.com>
2016-12-14 15:43:59 +0000

    Added runners to run individual scenarios

---
+++ src/test/java/com/mhra/mdcm/devices/appian/runners/RunSprint1Test.java
@@ -0,0 +1,19 @@
+package com.mhra.mdcm.devices.appian.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by TPD_Auto
+ * VM Options required : -ea -Dspring.profiles.active=mhratest -Dcurrent.browser=gc -Dgenerate.report=true
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:com.mhra.mdcm.devices.appian/stories/all_sprints",
+        glue = {"classpath:com.mhra.mdcm.devices.appian.steps","classpath:com.mhra.mdcm.devices.appian.steps.common"},
+        plugin = {"pretty", "html:target/cucumber-html-report", "json:target/sprint1_tests.json"}
+        , monochrome = true, tags = {"@sprint1"}
+)
+public class RunSprint1Test {
+}
---
+++ src/test/resources/com.mhra.mdcm.devices.appian/stories/others/run_specific_sprints.feature
@@ -0,0 +1,4 @@
+Feature: As a user I want to run all the stories related to a specific sprint
+  
+  Scenario: GAGA
+    Given I am logged into appian as "blah" user
