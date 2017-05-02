bb3ff56eb3736c889b9018c18ba06e4549883941
TPD_Auto <nooruddin1504@aol.com>
2017-05-02 14:30:41 +0100

    Runner to run tests from other directory

---
+++ src/test/java/com/mhra/mdcm/devices/appian/RunSpecificTest.java
@@ -0,0 +1,19 @@
+package com.mhra.mdcm.devices.appian;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by TPD_Auto
+ * VM Options required : -ea -Dspring.profiles.active=mhratest -Dcurrent.browser=gc -Dgenerate.report=true
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:com.mhra.mdcm.devices.appian/e2e",
+        glue = {"classpath:com.mhra.mdcm.devices.appian.steps","classpath:com.mhra.mdcm.devices.appian.steps.common"},
+        plugin = {"pretty", "html:target/cucumber-html-report", "json:target/cucumber.json", "rerun:target/failedScenarios.txt"}
+        , monochrome = true
+)
+public class RunSpecificTest {
+}
