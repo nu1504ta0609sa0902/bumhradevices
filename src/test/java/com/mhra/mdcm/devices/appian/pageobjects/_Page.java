67e5b5c69a3385435dcc4872fed5b802b5e9b209
TPD_Auto <nooruddin1504@aol.com>
2017-08-09 15:52:29 +0100

    Enabled default time which is normally very long

--- src/test/java/com/mhra/mdcm/devices/appian/pageobjects/_Page.java
+++ src/test/java/com/mhra/mdcm/devices/appian/pageobjects/_Page.java
@@ -14,15 +14,15 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration(locations = {"/cucumber.mhra.devices.xml"})
 public class _Page {
 
-    //This will wait for 1*60 seconds "Waiting..." message to dissappear, unless you set to 0
-    public static final int TIMEOUT_PAGE_LOAD = 2;
-    public static final boolean USE_DEFAULT_TIME_FOR_ALL = false;
-
     /**
      * Different timeouts for different actions: Use appropriately
      * Any pages which takes more than 10 seconds should be looked at
      **/
-    public static int TIMEOUT_DEFAULT = 60;
+    public static final boolean USE_DEFAULT_TIME_FOR_ALL = true;
+    public static int TIMEOUT_DEFAULT = 30;
+
+    //This will wait for 1*60 seconds "Waiting..." message to dissappear, unless you set to 0
+    public static final int TIMEOUT_PAGE_LOAD = 0;
 
     public static int TIMEOUT_1_SECOND = 1;
     public static int TIMEOUT_2_SECOND = 2;
